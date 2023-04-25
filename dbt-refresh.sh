823ff1aa2bf0d4d138c1fe121a416013bbb3bbb0
Bill <Bill@TheWallisFamily.co.uk>
2023-04-25 13:13:48 +0100

    ✨ feat: Add `dbt build` to commands

--- dbt-refresh.sh
+++ dbt-refresh.sh
@@ -2,7 +2,9 @@ dbt clean
 dbt deps
 dbt source freshness
 #dbt seed
-dbt run --full-refresh
-dbt test --store-failures
+#dbt run --full-refresh
+#dbt test --store-failures
+dbt build --full-refresh
+
 dbt docs generate
 dbt docs serve
