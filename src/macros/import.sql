a96a707543bc5425b1a658d8a1f583c27e70c954
Bill <Bill@TheWallisFamily.co.uk>
2023-04-25 08:01:09 +0100

    ✨ feat: Enable `import` macro

--- src/macros/import.sql
+++ src/macros/import.sql
@@ -1,10 +1,23 @@
-{# Import a set of objects #}
 
-{% macro import() %}
-    {%- for key, value in kwargs.items() %}
+{% macro import(
+    expand_columns=true,
+    include_with=true,
+    include_recursive=false,
+    include_final_comma=true
+) %}
+    {#
+    Expand the key-value pairs into CTEs whose names are the keys.
+    #}
+    {% if include_with -%}WITH{%- endif %}{% if include_recursive %} RECURSIVE{%- endif %}
+
+    {% for key, value in kwargs.items() %}
     {{ key }} AS (
-        {# Expand the relation #}
-        {{- dp_a4tb_demand_forecast_sales_forecast_poc_dv.select_star(value) | indent(width=8) }}
+        {% if expand_columns %}
+            {# Expand the columns of the relation #}
+            {{- select_star(value) | indent(width=8) }}
+        {% else %}
+            select * from {{ value }}
+        {% endif %}
     ){{- "" if loop.last else "," -}}
-    {% endfor %}
+    {% endfor %}{{- "," if include_final_comma else "" -}}
 {% endmacro %}
